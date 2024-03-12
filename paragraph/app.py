a90f7094fb168e2edd61c9986b6c50e841aba984
Teo <teo.simeonov@code.berlin>
2024-03-12 15:08:45 +0100

    top 7 WIP

--- paragraph/app.py
+++ paragraph/app.py
@@ -1,51 +1,48 @@
-from flask import Flask, redirect, url_for, send_file
+from flask import Flask, redirect, url_for, send_file, \
+    render_template, request, session, flash, g, make_response
 
 
+home = open("paragraph/index.html", "r")
+style = open("paragraph/styles/style.css", "r")
+about_page = open("paragraph/about.html", "r")
+app = Flask(__name__,)
+not_found = open("paragraph/not_found.html", "r")
+webpages_so_far = "home, about_page"
 
-app = Flask(__name__)
+top_seven_books = [
+    {'name': 'Why do we sleep: unlocking the power of sleep and dreams', 'rating': '4,9 stars'},
+    {'name': 'Make it stick: the science of successful learning', 'rating': '4,8 stars'},
+    {'name': 'Crice', 'rating': '4,89 stars'},
+    {'name': 'Girls against god', 'rating': '4,5 stars'},
+    {'name': 'These violent delights', 'rating': '4,45 stars'},
+    {'name': 'To paradise', 'rating': '4,13 stars'},
+    {'name': 'The greek myths: complete edition', 'rating': '4,12 stars'},
+]
 
 app.config.from_object('config')
 
-# cookies_data = {
-#     'chocolate-chip' : {'name': 'Chocolate Chip', 'price': '$1.50'},
-#     'oatmeal-raisin' : {'name': 'Oatmeal Raisin', 'price': '$1.00'},
-#     'sugar' : {'name': 'Sugar', 'price': '$0.75'},
-#     'peanut-butter' : {'name': 'Peanut Butter', 'price': '$0.50'},
-#     'oatmeal' : {'name': 'Oatmeal', 'price': '$0.25'},
-#     'salted-caramel' : {'name': 'Salted Caramel', 'price': '$1.00'},
-# }
-
-
-cookies_data = [
-    {'name': 'Chocolate Chip', 'price': '$1.50'},
-    {'name': 'Oatmeal Raisin', 'price': '$1.00'},
-    {'name': 'Sugar', 'price': '$0.75'},
-    {'name': 'Peanut Butter', 'price': '$0.50'},
-    {'name': 'Oatmeal', 'price': '$0.25'},
-    {'name': 'Salted Caramel', 'price': '$1.00'},
-]
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return home
+    
 
 @app.route('/about')
 def about():
-    return 'I like cookies'
+    return about_page
 
-@app.route('/about-me')
-def about_me():
-    return redirect(url_for('about'))
-
-@app.route('/cookies/<slug>')
-def cookie(slug):
-    if slug in cookies_data:
-        return '<h1>' + cookies_data[slug]['name'] + '</h1>' + '<p>' + cookies_data[slug]['price'] + '</p>'
+@app.route('/<slug>')
+def check(slug):
+    if slug in webpages_so_far:
+        return slug
     else:
-        return 'Sorry we could not find that cookie.' 
+        return not_found
+    
 
-@app.route('/cookies/<int:id>')
-def cookie(id):
-    return '<h1>' + cookies_data[id]['name'] + '</h1>' + '<p>' + cookies_data[id]['price'] + '</p>'
+# @app.route('/<slug>')
+# def book2(slug):
+#     if slug in top_seven_books:
+#         return '<h1>' + top_seven_books[slug]['name'] + '</h1>' + '<p>' + top_seven_books[slug]['price'] + '</p>'
+#     else:
+#         return 'Sorry we could not find that cookie.' 
 
 app.run()
